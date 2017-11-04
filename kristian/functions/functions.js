ff4ef934bbea677c8d07f7aba2ee30dbd1a1663e
krisKurken <kristian.liljander@live.se>
2017-11-04 14:35:04 +0100

    Added media query for func and style

--- kristian/functions/functions.js
+++ kristian/functions/functions.js
@@ -1,9 +1,18 @@
 function open_read_more2() {
-  document.getElementById("s2-readMore-id").style.width = "30%";
-  document.getElementById("s2-readMore-id").style.opacity = "1";
-  document.getElementById("section2-id").style.marginLeft = "30%";
-  document.getElementById("section2-textbox-id").style.left = "36%";
-  document.getElementById("s2-shade-layer-id").style.opacity = "0.7";
+  if (window.innerWidth < 500) {
+    document.getElementById("s2-readMore-id").style.width = "100%";
+    document.getElementById("section2-id").style.marginLeft = "100%";
+    document.getElementById("s2-readMore-id").style.opacity = "1";
+    document.getElementById("section2-textbox-id").style.left = "100%";
+  }
+  else {
+    document.getElementById("s2-readMore-id").style.width = "30%";
+    document.getElementById("s2-readMore-id").style.opacity = "1";
+    document.getElementById("section2-id").style.marginLeft = "30%";
+    document.getElementById("section2-textbox-id").style.left = "36%";
+    document.getElementById("s2-shade-layer-id").style.opacity = "0.7";
+  }
+
 }
 function close_read_more2() {
   document.getElementById("s2-readMore-id").style.width = "0";
