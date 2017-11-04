d42d04162a7bb1a4e36823fbed7a5b8f36ba5e5a
krisKurken <kristian.liljander@live.se>
2017-11-04 15:21:31 +0100

    Added responsive readmore section 3

--- functions/functions.js
+++ functions/functions.js
@@ -22,14 +22,23 @@ function close_read_more2() {
   document.getElementById("s2-shade-layer-id").style.opacity = "0";
 }
 function open_read_more3() {
-  document.getElementById("s3-readMore-id").style.width = "30%";
-  document.getElementById("s3-readMore-id").style.opacity = "1";
-  document.getElementById("section3-id").style.marginLeft = "30%";
-  document.getElementById("s3-shade-layer-id").style.opacity = "0.7";
+  if (window.innerWidth < 500) {
+    document.getElementById("s3-readMore-id").style.width = "100%";
+    document.getElementById("s3-readMore-id").style.opacity = "1";
+    document.getElementById("section3-id").style.marginLeft = "100%";
+    document.getElementById("section3-textbox-id").style.left = "100%";
+  }
+  else {
+    document.getElementById("s3-readMore-id").style.width = "30%";
+    document.getElementById("s3-readMore-id").style.opacity = "1";
+    document.getElementById("section3-id").style.marginLeft = "30%";
+    document.getElementById("s3-shade-layer-id").style.opacity = "0.7";
+  }
 }
 function close_read_more3() {
   document.getElementById("s3-readMore-id").style.width = "0";
   document.getElementById("s3-readMore-id").style.opacity = "0";
   document.getElementById("section3-id").style.marginLeft= "0";
   document.getElementById("s3-shade-layer-id").style.opacity = "0";
+  document.getElementById("section3-textbox-id").style.left = "66%";
 }
